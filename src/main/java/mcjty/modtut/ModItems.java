1436e2ed43d43296516378d2ee56af3a13f90d92
McJty <mcjty1@gmail.com>
2016-01-07 09:33:45 +0100

    Added simple textured item

--- src/main/java/mcjty/modtut/ModItems.java
+++ src/main/java/mcjty/modtut/ModItems.java
@@ -1,12 +1,23 @@
 package mcjty.modtut;
 
 import mcjty.modtut.items.FirstItem;
+import mcjty.modtut.items.SimpleTexturedItem;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ModItems {
 
     public static FirstItem firstItem;
+    public static SimpleTexturedItem simpleTexturedItem;
 
     public static void init() {
         firstItem = new FirstItem();
+        simpleTexturedItem = new SimpleTexturedItem();
     }
+
+    @SideOnly(Side.CLIENT)
+    public static void initModels() {
+        simpleTexturedItem.initModel();
+    }
+
 }
---
+++ src/main/java/mcjty/modtut/items/SimpleTexturedItem.java
@@ -0,0 +1,22 @@
+package mcjty.modtut.items;
+
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class SimpleTexturedItem extends Item {
+
+    public SimpleTexturedItem() {
+        setRegistryName("simpletextureditem");
+        setUnlocalizedName("simpletextureditem");
+        GameRegistry.registerItem(this);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+}
