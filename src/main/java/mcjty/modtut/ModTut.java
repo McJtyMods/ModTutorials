5ad6f5d447e10669f83cd423e75ed78a2ee0dcd8
McJty <mcjty1@gmail.com>
2016-01-07 08:48:50 +0100

    Basic mod class

--- src/main/java/mcjty/modtut/ModTut.java
+++ src/main/java/mcjty/modtut/ModTut.java
@@ -1,54 +1,31 @@
-package mcjty.gaia;
+package mcjty.modtut;
 
 
-import mcjty.gaia.blocks.tank.TankNetwork;
-import mcjty.gaia.environment.EnvironmentData;
-import mcjty.gaia.hosemultiblock.HoseNetwork;
-import mcjty.gaia.proxy.CommonProxy;
-import mcjty.gaia.waila.WailaCompatibility;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Random;
+@Mod(modid = ModTut.MODID, name = ModTut.MODNAME, dependencies = "required-after:Forge@[11.15.0.1684,)", useMetadata = true)
+public class ModTut {
 
-@Mod(modid = Gaia.MODID, name = Gaia.ModName, dependencies = "required-after:Forge@[11.15.0.1684,)", useMetadata = true)
-public class Gaia {
+    public static final String MODID = "modtut";
+    public static final String MODNAME = "Mod tutorials";
 
-    public static final String MODID = "gaia";
-    public static final String ModName = "Gaia Realism";
-
-    @SidedProxy(clientSide = "mcjty.gaia.proxy.ClientProxy", serverSide = "mcjty.gaia.proxy.CommonProxy")
+    @SidedProxy
     public static CommonProxy proxy;
 
     @Mod.Instance
-    public static Gaia instance;
+    public static ModTut instance;
+
     public static Logger logger;
-    public static CreativeTabs creativeTab;
-    public static Random random;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
         logger = event.getModLog();
-        creativeTab = new CreativeTabs("gaia") {
-            @Override
-            public Item getTabIconItem() {
-                return Items.apple;
-            }
-        };
-        random = new Random();
         proxy.preInit(event);
-
-        WailaCompatibility.registerWaila();
     }
 
     @Mod.EventHandler
@@ -61,20 +38,30 @@ public class Gaia {
         proxy.postInit(e);
     }
 
-    @Mod.EventHandler
-    public void serverStopped(FMLServerStoppedEvent event) {
-        logger.log(Level.INFO, "Gaia: server is stopping. Shutting down gracefully");
-        TankNetwork.clearInstance();
-        HoseNetwork.clearInstance();
-        EnvironmentData.clearInstance();
+    public static class CommonProxy {
+        public void preInit(FMLPreInitializationEvent e) {
+            // Initialization of blocks and items typically goes here:
+        }
+
+        public void init(FMLInitializationEvent e) {
+
+        }
+
+        public void postInit(FMLPostInitializationEvent e) {
+
+        }
     }
 
-    private static void registerForgeAndFML(Object o){
-        registerForge(o);
+
+    public static class ClientProxy extends CommonProxy {
+        @Override
+        public void preInit(FMLPreInitializationEvent e) {
+            super.preInit(e);
+            // Typically initialization of models and such goes here:
+        }
     }
 
-    private static void registerForge(Object o){
-        MinecraftForge.EVENT_BUS.register(o);
-    }
+    public static class ServerProxy extends CommonProxy {
 
+    }
 }
