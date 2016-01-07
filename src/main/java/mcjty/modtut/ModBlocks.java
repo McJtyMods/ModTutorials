5ac03111121cd79eada08f84e52867ee8c5236f6
McJty <mcjty1@gmail.com>
2016-01-07 13:36:27 +0100

    State textured block

--- src/main/java/mcjty/modtut/ModBlocks.java
+++ src/main/java/mcjty/modtut/ModBlocks.java
@@ -3,6 +3,7 @@ package mcjty.modtut;
 import mcjty.modtut.blocks.FirstBlock;
 import mcjty.modtut.blocks.MultiTexturedBlock;
 import mcjty.modtut.blocks.SimpleTexturedBlock;
+import mcjty.modtut.blocks.StateTexturedBlock;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -11,17 +12,20 @@ public class ModBlocks {
     public static FirstBlock firstBlock;
     public static SimpleTexturedBlock simpleTexturedBlock;
     public static MultiTexturedBlock multiTexturedBlock;
+    public static StateTexturedBlock stateTexturedBlock;
 
     public static void init() {
         firstBlock = new FirstBlock();
         simpleTexturedBlock = new SimpleTexturedBlock();
         multiTexturedBlock = new MultiTexturedBlock();
+        stateTexturedBlock = new StateTexturedBlock();
     }
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         simpleTexturedBlock.initModel();
         multiTexturedBlock.initModel();
+        stateTexturedBlock.initModel();
     }
 
 }
---
+++ src/main/java/mcjty/modtut/blocks/StateTexturedBlock.java
@@ -0,0 +1,72 @@
+package mcjty.modtut.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class StateTexturedBlock extends Block {
+
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
+    public static final PropertyBool ENABLED = PropertyBool.create("enabled");
+
+
+    public StateTexturedBlock() {
+        super(Material.rock);
+        setUnlocalizedName("statetexturedblock");
+        setRegistryName("statetexturedblock");
+        GameRegistry.registerBlock(this);
+
+        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+    @Override
+    public void onNeighborBlockChange(World world, BlockPos pos, IBlockState state, Block neighborBlock) {
+        int powered = world.isBlockIndirectlyGettingPowered(pos);
+        world.setBlockState(pos, state.withProperty(ENABLED, powered > 0), 3);
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        world.setBlockState(pos, state.withProperty(FACING, getFacingFromEntity(pos, placer)), 2);
+    }
+
+    public static EnumFacing getFacingFromEntity(BlockPos clickedBlock, EntityLivingBase entity) {
+        return EnumFacing.getFacingFromVector((float) (entity.posX - clickedBlock.getX()), (float) (entity.posY - clickedBlock.getY()), (float) (entity.posZ - clickedBlock.getZ()));
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return getDefaultState()
+                .withProperty(FACING, EnumFacing.getFront(meta & 7))
+                .withProperty(ENABLED, (meta & 8) != 0);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex() + (state.getValue(ENABLED) ? 8 : 0);
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, FACING, ENABLED);
+    }
+}
