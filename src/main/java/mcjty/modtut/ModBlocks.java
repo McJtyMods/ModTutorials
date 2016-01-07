8395a75228da07eeaafb186d048de91aab35282e
McJty <mcjty1@gmail.com>
2016-01-07 12:02:33 +0100

    Added the multi-textured block

--- src/main/java/mcjty/modtut/ModBlocks.java
+++ src/main/java/mcjty/modtut/ModBlocks.java
@@ -1,6 +1,7 @@
 package mcjty.modtut;
 
 import mcjty.modtut.blocks.FirstBlock;
+import mcjty.modtut.blocks.MultiTexturedBlock;
 import mcjty.modtut.blocks.SimpleTexturedBlock;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -9,15 +10,18 @@ public class ModBlocks {
 
     public static FirstBlock firstBlock;
     public static SimpleTexturedBlock simpleTexturedBlock;
+    public static MultiTexturedBlock multiTexturedBlock;
 
     public static void init() {
         firstBlock = new FirstBlock();
         simpleTexturedBlock = new SimpleTexturedBlock();
+        multiTexturedBlock = new MultiTexturedBlock();
     }
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         simpleTexturedBlock.initModel();
+        multiTexturedBlock.initModel();
     }
 
 }
---
+++ src/main/java/mcjty/modtut/blocks/MultiTexturedBlock.java
@@ -0,0 +1,62 @@
+package mcjty.modtut.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class MultiTexturedBlock extends Block {
+
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
+
+
+    public MultiTexturedBlock() {
+        super(Material.rock);
+        setUnlocalizedName("multitexturedblock");
+        setRegistryName("multitexturedblock");
+        GameRegistry.registerBlock(this);
+
+        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        world.setBlockState(pos, state.withProperty(FACING, getFacingFromEntity(pos, placer)), 2);
+    }
+
+    public static EnumFacing getFacingFromEntity(BlockPos clickedBlock, EntityLivingBase entity) {
+        return EnumFacing.getFacingFromVector((float) (entity.posX - clickedBlock.getX()), (float) (entity.posY - clickedBlock.getY()), (float) (entity.posZ - clickedBlock.getZ()));
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex();
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, FACING);
+    }
+}
