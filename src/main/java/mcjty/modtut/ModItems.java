843d49542d39322fe104f61a2fe4057a54171855
McJty <mcjty1@gmail.com>
2016-01-07 11:30:59 +0100

    Added a multi-model item

--- src/main/java/mcjty/modtut/ModItems.java
+++ src/main/java/mcjty/modtut/ModItems.java
@@ -1,6 +1,7 @@
 package mcjty.modtut;
 
 import mcjty.modtut.items.FirstItem;
+import mcjty.modtut.items.MultiModelItem;
 import mcjty.modtut.items.SimpleTexturedItem;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -9,15 +10,18 @@ public class ModItems {
 
     public static FirstItem firstItem;
     public static SimpleTexturedItem simpleTexturedItem;
+    public static MultiModelItem multiModelItem;
 
     public static void init() {
         firstItem = new FirstItem();
         simpleTexturedItem = new SimpleTexturedItem();
+        multiModelItem = new MultiModelItem();
     }
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         simpleTexturedItem.initModel();
+        multiModelItem.initModel();
     }
 
 }
---
+++ src/main/java/mcjty/modtut/items/MultiModelItem.java
@@ -0,0 +1,67 @@
+package mcjty.modtut.items;
+
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class MultiModelItem extends Item {
+
+    public MultiModelItem() {
+        setRegistryName("multimodelitem");
+        setUnlocalizedName("multimodelitem");
+        GameRegistry.registerItem(this);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelResourceLocation blueModel = new ModelResourceLocation(getRegistryName() + "_blue", "inventory");
+        ModelResourceLocation redModel = new ModelResourceLocation(getRegistryName() + "_red", "inventory");
+
+        ModelBakery.registerItemVariants(this, blueModel, redModel);
+
+        ModelLoader.setCustomMeshDefinition(this, new ItemMeshDefinition() {
+            @Override
+            public ModelResourceLocation getModelLocation(ItemStack stack) {
+                if (isBlue(stack)) {
+                    return blueModel;
+                } else {
+                    return redModel;
+                }
+            }
+        });
+    }
+
+    private boolean isBlue(ItemStack stack) {
+        return getTagCompoundSafe(stack).hasKey("blue");
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+        if (!world.isRemote) {
+            if (isBlue(stack)) {
+                getTagCompoundSafe(stack).removeTag("blue");
+            } else {
+                getTagCompoundSafe(stack).setBoolean("blue", true);
+            }
+        }
+        return stack;
+    }
+
+    private NBTTagCompound getTagCompoundSafe(ItemStack stack) {
+        NBTTagCompound tagCompound = stack.getTagCompound();
+        if (tagCompound == null) {
+            tagCompound = new NBTTagCompound();
+            stack.setTagCompound(tagCompound);
+        }
+        return tagCompound;
+    }
+}
