df9bdc2f99ad77cfabb9490b1c21849f05d8b41a
McJty <mcjty1@gmail.com>
2016-04-25 11:25:31 +0200

    Use the builder for quads

--- src/main/java/mcjty/modtut/blocks/bakedmodel/ExampleBakedModel.java
+++ src/main/java/mcjty/modtut/blocks/bakedmodel/ExampleBakedModel.java
@@ -1,6 +1,5 @@
 package mcjty.modtut.blocks.bakedmodel;
 
-import com.google.common.primitives.Ints;
 import mcjty.modtut.ModTut;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
@@ -10,6 +9,7 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.model.pipeline.LightUtil;
+import net.minecraftforge.client.model.pipeline.UnpackedBakedQuad;
 import net.minecraftforge.common.property.IExtendedBlockState;
 
 import java.util.ArrayList;
@@ -21,28 +21,44 @@ public class ExampleBakedModel implements IBakedModel {
 
     private TextureAtlasSprite sprite = Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(ModTut.MODID + ":blocks/isbmtexture");
 
-    private int[] vertexToInts(double x, double y, double z, float u, float v, TextureAtlasSprite sprite) {
-        return new int[] {
-                Float.floatToRawIntBits((float) x),
-                Float.floatToRawIntBits((float) y),
-                Float.floatToRawIntBits((float) z),
-                -1,
-                Float.floatToRawIntBits(sprite.getInterpolatedU(u)),
-                Float.floatToRawIntBits(sprite.getInterpolatedV(v)),
-                0
-        };
+    private void putVertex(UnpackedBakedQuad.Builder builder, EnumFacing side, double x, double y, double z, float u, float v) {
+        for (int e = 0; e < DefaultVertexFormats.ITEM.getElementCount(); e++) {
+            switch (DefaultVertexFormats.ITEM.getElement(e).getUsage()) {
+                case POSITION:
+                    float[] data = new float[]{(float)x, (float)y, (float)z, 1};
+                    builder.put(e, data);
+                    break;
+                case COLOR:
+                    builder.put(e, 0xffffffff);
+                    break;
+                case UV:
+                    if (DefaultVertexFormats.ITEM.getElement(e).getIndex() == 0) {
+                        u = sprite.getInterpolatedU(u);
+                        v = sprite.getInterpolatedV(v);
+                        builder.put(e, u, v, 0f, 1f);
+                        break;
+                    }
+                case NORMAL:
+                    builder.put(e, side.getFrontOffsetX(), side.getFrontOffsetY(), side.getFrontOffsetZ(), 0f);
+                    break;
+                default:
+                    builder.put(e);
+                    break;
+            }
+        }
     }
 
     private BakedQuad createQuad(Vec3d v1, Vec3d v2, Vec3d v3, Vec3d v4, TextureAtlasSprite sprite) {
         Vec3d normal = v1.subtract(v2).crossProduct(v3.subtract(v2));
         EnumFacing side = LightUtil.toSide((float) normal.xCoord, (float) normal.yCoord, (float) normal.zCoord);
 
-        return new BakedQuad(Ints.concat(
-                vertexToInts(v1.xCoord, v1.yCoord, v1.zCoord, 0, 0, sprite),
-                vertexToInts(v2.xCoord, v2.yCoord, v2.zCoord, 0, 16, sprite),
-                vertexToInts(v3.xCoord, v3.yCoord, v3.zCoord, 16, 16, sprite),
-                vertexToInts(v4.xCoord, v4.yCoord, v4.zCoord, 16, 0, sprite)
-        ), -1, side, sprite, true, DefaultVertexFormats.ITEM);
+        UnpackedBakedQuad.Builder builder = new UnpackedBakedQuad.Builder(DefaultVertexFormats.ITEM);
+        builder.setTexture(sprite);
+        putVertex(builder, side, v1.xCoord, v1.yCoord, v1.zCoord, 0, 0);
+        putVertex(builder, side, v2.xCoord, v2.yCoord, v2.zCoord, 0, 16);
+        putVertex(builder, side, v3.xCoord, v3.yCoord, v3.zCoord, 16, 16);
+        putVertex(builder, side, v4.xCoord, v4.yCoord, v4.zCoord, 16, 0);
+        return builder.build();
     }
 
     @Override
@@ -61,56 +77,56 @@ public class ExampleBakedModel implements IBakedModel {
         // or else we extend so that we touch the adjacent block:
 
         if (up) {
-            quads.add(createQuad(new Vec3d(1-o, 1-o, o), new Vec3d(1-o, 1, o), new Vec3d(1-o, 1, 1-o), new Vec3d(1-o, 1-o, 1-o), sprite));
-            quads.add(createQuad(new Vec3d(o, 1-o, 1-o), new Vec3d(o, 1, 1-o), new Vec3d(o, 1, o), new Vec3d(o, 1-o, o), sprite));
-            quads.add(createQuad(new Vec3d(o, 1, o), new Vec3d(1-o, 1, o), new Vec3d(1-o, 1-o, o), new Vec3d(o, 1-o, o), sprite));
-            quads.add(createQuad(new Vec3d(o, 1-o, 1-o), new Vec3d(1-o, 1-o, 1-o), new Vec3d(1-o, 1, 1-o), new Vec3d(o, 1, 1-o), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, 1 - o, o), new Vec3d(1 - o, 1, o), new Vec3d(1 - o, 1, 1 - o), new Vec3d(1 - o, 1 - o, 1 - o), sprite));
+            quads.add(createQuad(new Vec3d(o, 1 - o, 1 - o), new Vec3d(o, 1, 1 - o), new Vec3d(o, 1, o), new Vec3d(o, 1 - o, o), sprite));
+            quads.add(createQuad(new Vec3d(o, 1, o), new Vec3d(1 - o, 1, o), new Vec3d(1 - o, 1 - o, o), new Vec3d(o, 1 - o, o), sprite));
+            quads.add(createQuad(new Vec3d(o, 1 - o, 1 - o), new Vec3d(1 - o, 1 - o, 1 - o), new Vec3d(1 - o, 1, 1 - o), new Vec3d(o, 1, 1 - o), sprite));
         } else {
-            quads.add(createQuad(new Vec3d(o, 1-o, 1-o), new Vec3d(1-o, 1-o, 1-o), new Vec3d(1-o, 1-o, o), new Vec3d(o, 1-o, o), sprite));
+            quads.add(createQuad(new Vec3d(o, 1 - o, 1 - o), new Vec3d(1 - o, 1 - o, 1 - o), new Vec3d(1 - o, 1 - o, o), new Vec3d(o, 1 - o, o), sprite));
         }
 
         if (down) {
-            quads.add(createQuad(new Vec3d(1-o, 0, o), new Vec3d(1-o, o, o), new Vec3d(1-o, o, 1-o), new Vec3d(1-o, 0, 1-o), sprite));
-            quads.add(createQuad(new Vec3d(o, 0, 1-o), new Vec3d(o, o, 1-o), new Vec3d(o, o, o), new Vec3d(o, 0, o), sprite));
-            quads.add(createQuad(new Vec3d(o, o, o), new Vec3d(1-o, o, o), new Vec3d(1-o, 0, o), new Vec3d(o, 0, o), sprite));
-            quads.add(createQuad(new Vec3d(o, 0, 1-o), new Vec3d(1-o, 0, 1-o), new Vec3d(1-o, o, 1-o), new Vec3d(o, o, 1-o), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, 0, o), new Vec3d(1 - o, o, o), new Vec3d(1 - o, o, 1 - o), new Vec3d(1 - o, 0, 1 - o), sprite));
+            quads.add(createQuad(new Vec3d(o, 0, 1 - o), new Vec3d(o, o, 1 - o), new Vec3d(o, o, o), new Vec3d(o, 0, o), sprite));
+            quads.add(createQuad(new Vec3d(o, o, o), new Vec3d(1 - o, o, o), new Vec3d(1 - o, 0, o), new Vec3d(o, 0, o), sprite));
+            quads.add(createQuad(new Vec3d(o, 0, 1 - o), new Vec3d(1 - o, 0, 1 - o), new Vec3d(1 - o, o, 1 - o), new Vec3d(o, o, 1 - o), sprite));
         } else {
-            quads.add(createQuad(new Vec3d(o, o, o), new Vec3d(1-o, o, o), new Vec3d(1-o, o, 1-o), new Vec3d(o, o, 1-o), sprite));
+            quads.add(createQuad(new Vec3d(o, o, o), new Vec3d(1 - o, o, o), new Vec3d(1 - o, o, 1 - o), new Vec3d(o, o, 1 - o), sprite));
         }
 
         if (east) {
-            quads.add(createQuad(new Vec3d(1-o, 1-o, 1-o), new Vec3d(1, 1-o, 1-o), new Vec3d(1, 1-o, o), new Vec3d(1-o, 1-o, o), sprite));
-            quads.add(createQuad(new Vec3d(1-o, o, o), new Vec3d(1, o, o), new Vec3d(1, o, 1-o), new Vec3d(1-o, o, 1-o), sprite));
-            quads.add(createQuad(new Vec3d(1-o, 1-o, o), new Vec3d(1, 1-o, o), new Vec3d(1, o, o), new Vec3d(1-o, o, o), sprite));
-            quads.add(createQuad(new Vec3d(1-o, o, 1-o), new Vec3d(1, o, 1-o), new Vec3d(1, 1-o, 1-o), new Vec3d(1-o, 1-o, 1-o), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, 1 - o, 1 - o), new Vec3d(1, 1 - o, 1 - o), new Vec3d(1, 1 - o, o), new Vec3d(1 - o, 1 - o, o), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, o, o), new Vec3d(1, o, o), new Vec3d(1, o, 1 - o), new Vec3d(1 - o, o, 1 - o), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, 1 - o, o), new Vec3d(1, 1 - o, o), new Vec3d(1, o, o), new Vec3d(1 - o, o, o), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, o, 1 - o), new Vec3d(1, o, 1 - o), new Vec3d(1, 1 - o, 1 - o), new Vec3d(1 - o, 1 - o, 1 - o), sprite));
         } else {
-            quads.add(createQuad(new Vec3d(1-o, o, o), new Vec3d(1-o, 1-o, o), new Vec3d(1-o, 1-o, 1-o), new Vec3d(1-o, o, 1-o), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, o, o), new Vec3d(1 - o, 1 - o, o), new Vec3d(1 - o, 1 - o, 1 - o), new Vec3d(1 - o, o, 1 - o), sprite));
         }
 
         if (west) {
-            quads.add(createQuad(new Vec3d(0, 1-o, 1-o), new Vec3d(o, 1-o, 1-o), new Vec3d(o, 1-o, o), new Vec3d(0, 1-o, o), sprite));
-            quads.add(createQuad(new Vec3d(0, o, o), new Vec3d(o, o, o), new Vec3d(o, o, 1-o), new Vec3d(0, o, 1-o), sprite));
-            quads.add(createQuad(new Vec3d(0, 1-o, o), new Vec3d(o, 1-o, o), new Vec3d(o, o, o), new Vec3d(0, o, o), sprite));
-            quads.add(createQuad(new Vec3d(0, o, 1-o), new Vec3d(o, o, 1-o), new Vec3d(o, 1-o, 1-o), new Vec3d(0, 1-o, 1-o), sprite));
+            quads.add(createQuad(new Vec3d(0, 1 - o, 1 - o), new Vec3d(o, 1 - o, 1 - o), new Vec3d(o, 1 - o, o), new Vec3d(0, 1 - o, o), sprite));
+            quads.add(createQuad(new Vec3d(0, o, o), new Vec3d(o, o, o), new Vec3d(o, o, 1 - o), new Vec3d(0, o, 1 - o), sprite));
+            quads.add(createQuad(new Vec3d(0, 1 - o, o), new Vec3d(o, 1 - o, o), new Vec3d(o, o, o), new Vec3d(0, o, o), sprite));
+            quads.add(createQuad(new Vec3d(0, o, 1 - o), new Vec3d(o, o, 1 - o), new Vec3d(o, 1 - o, 1 - o), new Vec3d(0, 1 - o, 1 - o), sprite));
         } else {
-            quads.add(createQuad(new Vec3d(o, o, 1-o), new Vec3d(o, 1-o, 1-o), new Vec3d(o, 1-o, o), new Vec3d(o, o, o), sprite));
+            quads.add(createQuad(new Vec3d(o, o, 1 - o), new Vec3d(o, 1 - o, 1 - o), new Vec3d(o, 1 - o, o), new Vec3d(o, o, o), sprite));
         }
 
         if (north) {
-            quads.add(createQuad(new Vec3d(o, 1-o, o), new Vec3d(1-o, 1-o, o), new Vec3d(1-o, 1-o, 0), new Vec3d(o, 1-o, 0), sprite));
-            quads.add(createQuad(new Vec3d(o, o, 0), new Vec3d(1-o, o, 0), new Vec3d(1-o, o, o), new Vec3d(o, o, o), sprite));
-            quads.add(createQuad(new Vec3d(1-o, o, 0), new Vec3d(1-o, 1-o, 0), new Vec3d(1-o, 1-o, o), new Vec3d(1-o, o, o), sprite));
-            quads.add(createQuad(new Vec3d(o, o, o), new Vec3d(o, 1-o, o), new Vec3d(o, 1-o, 0), new Vec3d(o, o, 0), sprite));
+            quads.add(createQuad(new Vec3d(o, 1 - o, o), new Vec3d(1 - o, 1 - o, o), new Vec3d(1 - o, 1 - o, 0), new Vec3d(o, 1 - o, 0), sprite));
+            quads.add(createQuad(new Vec3d(o, o, 0), new Vec3d(1 - o, o, 0), new Vec3d(1 - o, o, o), new Vec3d(o, o, o), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, o, 0), new Vec3d(1 - o, 1 - o, 0), new Vec3d(1 - o, 1 - o, o), new Vec3d(1 - o, o, o), sprite));
+            quads.add(createQuad(new Vec3d(o, o, o), new Vec3d(o, 1 - o, o), new Vec3d(o, 1 - o, 0), new Vec3d(o, o, 0), sprite));
         } else {
-            quads.add(createQuad(new Vec3d(o, 1-o, o), new Vec3d(1-o, 1-o, o), new Vec3d(1-o, o, o), new Vec3d(o, o, o), sprite));
+            quads.add(createQuad(new Vec3d(o, 1 - o, o), new Vec3d(1 - o, 1 - o, o), new Vec3d(1 - o, o, o), new Vec3d(o, o, o), sprite));
         }
         if (south) {
-            quads.add(createQuad(new Vec3d(o, 1-o, 1), new Vec3d(1-o, 1-o, 1), new Vec3d(1-o, 1-o, 1-o), new Vec3d(o, 1-o, 1-o), sprite));
-            quads.add(createQuad(new Vec3d(o, o, 1-o), new Vec3d(1-o, o, 1-o), new Vec3d(1-o, o, 1), new Vec3d(o, o, 1), sprite));
-            quads.add(createQuad(new Vec3d(1-o, o, 1-o), new Vec3d(1-o, 1-o, 1-o), new Vec3d(1-o, 1-o, 1), new Vec3d(1-o, o, 1), sprite));
-            quads.add(createQuad(new Vec3d(o, o, 1), new Vec3d(o, 1-o, 1), new Vec3d(o, 1-o, 1-o), new Vec3d(o, o, 1-o), sprite));
+            quads.add(createQuad(new Vec3d(o, 1 - o, 1), new Vec3d(1 - o, 1 - o, 1), new Vec3d(1 - o, 1 - o, 1 - o), new Vec3d(o, 1 - o, 1 - o), sprite));
+            quads.add(createQuad(new Vec3d(o, o, 1 - o), new Vec3d(1 - o, o, 1 - o), new Vec3d(1 - o, o, 1), new Vec3d(o, o, 1), sprite));
+            quads.add(createQuad(new Vec3d(1 - o, o, 1 - o), new Vec3d(1 - o, 1 - o, 1 - o), new Vec3d(1 - o, 1 - o, 1), new Vec3d(1 - o, o, 1), sprite));
+            quads.add(createQuad(new Vec3d(o, o, 1), new Vec3d(o, 1 - o, 1), new Vec3d(o, 1 - o, 1 - o), new Vec3d(o, o, 1 - o), sprite));
         } else {
-            quads.add(createQuad(new Vec3d(o, o, 1-o), new Vec3d(1-o, o, 1-o), new Vec3d(1-o, 1-o, 1-o), new Vec3d(o, 1-o, 1-o), sprite));
+            quads.add(createQuad(new Vec3d(o, o, 1 - o), new Vec3d(1 - o, o, 1 - o), new Vec3d(1 - o, 1 - o, 1 - o), new Vec3d(o, 1 - o, 1 - o), sprite));
         }
 
         return quads;
@@ -143,6 +159,6 @@ public class ExampleBakedModel implements IBakedModel {
 
     @Override
     public ItemCameraTransforms getItemCameraTransforms() {
-            return ItemCameraTransforms.DEFAULT;
-        }
+        return ItemCameraTransforms.DEFAULT;
+    }
 }
